82a40e1f594b42245a1ff1ddf79051a2c4eebbd6
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-26 15:29:33 +0300

    Find external API

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+const apiUrl = "https://api.tvmaze.com/shows";
+const likesApiUrl =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EK8AqlUP7MtIYG7gJYqn/likes/";
