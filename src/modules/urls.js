5d99fe19acfe9be2fa9e5929cc373dca5c45e548
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-26 16:21:05 +0300

    Cleanup unsused code

--- src/modules/urls.js
+++
@@ -1,6 +0,0 @@
-const gameId = '6EJ4v384D0KMWfgikijg';
-const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-
-const url = `${baseURL}games/${gameId}/scores/`;
-
-export default url;
--- src/modules/validator.js
+++
@@ -1,6 +0,0 @@
-const inputsAreValid = (name, score) => {
-  if (name === '' || Number.isNaN(+score)) return false;
-  return true;
-};
-
-export default inputsAreValid;
