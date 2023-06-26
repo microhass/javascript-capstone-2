e810913db87fbcf737b680ba0b773892a0bc6ddd
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-26 21:41:33 +0300

    Footer-add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.css';
+// const apiUrl = 'https://api.tvmaze.com/shows';
+// const likesApiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EK8AqlUP7MtIYG7gJYqn/likes/';
