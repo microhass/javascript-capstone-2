5c0bcaada5f40d5f9abd78c023b143eae8fdc99f
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-26 15:47:48 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "./style.css";
-
-const apiUrl = "https://api.tvmaze.com/shows";
-const likesApiUrl =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EK8AqlUP7MtIYG7gJYqn/likes/";
+// const apiUrl = "https://api.tvmaze.com/shows";
+// const likesApiUrl =
+//   "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EK8AqlUP7MtIYG7gJYqn/likes/";
