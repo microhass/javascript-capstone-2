9adc09a132bbf349348de4f7d8206113f1410202
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-27 10:52:42 +0300

    Fetch movies and render functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import * as api from './modules/api.js';
+import renderShows from './modules/view.js';
 import './style.css';
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const shows = await api.getShows();
+  renderShows(shows);
+});
---
+++ src/modules/urls.js
@@ -0,0 +1,7 @@
+const showsUrl = 'https://api.tvmaze.com/shows';
+const appId = 'qNQntxTmv7kNJmha8EWq';
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const likesUrl = `${baseUrl}/apps/${appId}/likes/`;
+const commentsUrl = `${baseUrl}/apps/${appId}/comments/`;
+
+export { showsUrl, likesUrl, commentsUrl };
