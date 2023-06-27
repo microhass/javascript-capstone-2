b06008522bc5dbd39df8c835e34b097c7d78e3f2
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-27 11:26:46 +0300

    Fixed linter errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 import './style.css';
-// const apiUrl = 'https://api.tvmaze.com/shows';
-// const likesApiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/EK8AqlUP7MtIYG7gJYqn/likes/';
